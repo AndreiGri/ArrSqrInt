4e1e9bb6fed7062e0f4be447e2fd6d5d85002b26
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-11 11:29:15 +0500

    Добавьте файлы проекта.

---
+++ ArrSqrInt/ArrSqrInt.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > nul");
+    int n;
+    const int k = 10;
+    cout << "Введите натуральное число: ";
+    cin >> n;
+    int arr[k];
+    int i = 0;
+    while (i < k) {
+        arr[i] = n * n;
+        cout << arr[i] <<" ";
+        i++;
+        n++;
+    }
+    system("pause > nul");
+    return 0;
+
+}
